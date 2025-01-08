0ca5663e62fa9ddda061e1914f8b52756e14513b
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-01-08 20:30:20 +0530

    Added components

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,1 +1,8 @@
-<section id="about"></section>
+<script>
+    export let message = "Hello from MyComponent!";
+</script>
+
+<div style="padding: 1rem; border: 1px solid #ccc;">
+    <p>{message}
+    </p>
+</div>
