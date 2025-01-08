e81560ce2474f217a4c4f23f0fa6d18a73c1964b
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2025-01-08 11:59:37 +0530

    routes added

---
+++ src/routes/(tracksgrp)/[slug]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<h1>{data.post.title}</h1>
+<div>{@html data.post.content}</div>
